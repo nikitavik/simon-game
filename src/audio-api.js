b149116af7b063b08334195cbf2f0d9191944f07
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-07-03 18:51:38 +0300

    Fixed building error

--- src/audio-api.js
+++ src/audio-api.js
@@ -1,9 +1,9 @@
 class SoundApi {
   static sounds ={
-    blue: new Audio('/assets/sounds_1.mp3'),
-    red: new Audio('/assets/sounds_2.mp3'),
-    yellow: new Audio('/assets/sounds_3.mp3'),
-    green: new Audio('/assets/sounds_4.mp3')
+    blue: new Audio('./assets/sounds_1.mp3'),
+    red: new Audio('./assets/sounds_2.mp3'),
+    yellow: new Audio('./assets/sounds_3.mp3'),
+    green: new Audio('./assets/sounds_4.mp3')
   }
 
   static playSound (color) {
