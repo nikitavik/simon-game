68dfbeab8cdb50c0a44bbb9b224173d369269153
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-07-03 19:40:00 +0300

    Move StartButton to separated component

---
+++ src/components/StartButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button
+      @click="$emit('click-start')"
+      class="start-button">{{ !active ? "Start" : "Stop" }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartButton',
+  emits: {
+    'click-start': null
+  },
+  props: {
+    active: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*Start Button*/
+.start-button{
+  padding: 1rem 4rem;
+  border: 2px solid black;
+  background-color: lightcoral;
+  cursor: pointer;
+}
+</style>
